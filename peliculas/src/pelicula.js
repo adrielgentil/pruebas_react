98ba6fa1cc82f585ef982beac66dbcea478bd347
adrielgentil <adrielgentil@gmail.com>
2024-01-08 14:40:36 -0300

    PageWrapper component

--- peliculas/src/pelicula.js
+++ peliculas/src/pelicula.js
@@ -1,14 +1,14 @@
-export default function Peliculas() {
+export default function Pelicula(props) {
     return (
         <div className="movie-item-style-2">
-            <img src="images/uploads/mv1.jpg" alt="" />
+            <img src={props.image} alt="" />
             <div className="mv-item-infor">
-                <h6><a href="moviesingle.html">oblivion <span>(2012)</span></a></h6>
-                <p className="rate"><i className="ion-android-star"></i><span>8.1</span> /10</p>
-                <p className="describe">Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity...</p>
-                <p className="run-time"> Run Time: 2h21’    .     <span>MMPA: PG-13 </span>    .     <span>Release: 1 May 2015</span></p>
-                <p>Director: <a href="#">Joss Whedon</a></p>
-                <p>Stars: <a href="#">Robert Downey Jr.,</a> <a href="#">Chris Evans,</a> <a href="#">  Chris Hemsworth</a></p>
+                <h6><a href="moviesingle.html">{props.title} <span>({props.year})</span></a></h6>
+                <p className="rate"><i className="ion-android-star"></i><span>{props.score}</span> /10</p>
+                <p className="describe">{props.children}</p>
+                <p className="run-time"> Run Time: {props.duration}’    .     <span>MMPA: PG-13 </span>    .     <span>Release: {props.release}</span></p>
+                <p>Director: <a href="#">{props.director}</a></p>
+                <p>Stars: {props.cast}</p>
             </div>
         </div>
     );
